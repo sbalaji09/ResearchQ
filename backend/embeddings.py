20dafa132550d3c41811d467e68f5d2d2b127f85
Siddharth Balaji <siddharthbalaji6@gmail.com>
2025-12-12 23:56:48 -0800

    feat: implemented function to store vectors in pinecone

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -1,7 +1,9 @@
 from openai import OpenAI
 import os
+from pinecone import Pinecone, ServerlessSpec
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
 
 # gets the embedding for one piece of text
 def get_embedding(text: str):
@@ -41,4 +43,20 @@ def embed_chunks(chunks: list[str], metadata: list[dict], batch_size: int = 64):
 
     return vectors
 
+# stores vectors in pinecone based on a batch limit
+def store_in_pinecone(vectors, batch_limit=100):
+    index_name = os.environ.get("PINECONE_INDEX_NAME")
+    index = pc.Index(index_name)
 
+    upsert_data = []
+    for vector_data in vectors:
+        upsert_data.append(
+            (vector_data["id"], vector_data["values"], vector_data("metadata"))
+        )
+
+        if len(upsert_data) >= batch_limit:
+            index.upsert(vectors=upsert_data)
+            upsert_data = []
+
+    if upsert_data:    
+        index.upsert(vectors=upsert_data)
