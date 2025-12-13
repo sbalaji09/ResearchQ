32600ceccd40fd37ade8b828701cfee3dc9d2633
Siddharth Balaji <siddharthbalaji6@gmail.com>
2025-12-12 23:52:14 -0800

    feat: implemented chunk embedder to embed chunks of pdfs

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -15,3 +15,30 @@ def get_embedding(text: str):
     )
 
     return response.data[0].embedding
+
+# embeds chunks of data using OpenAI embedding model
+def embed_chunks(chunks: list[str], metadata: list[dict], batch_size: int = 64):
+    if len(chunks) != len(metadata):
+        return
+    
+    vectors = []
+
+    for start in range(0, len(chunks), batch_size):
+        end = min(start+batch_size, len(chunks))
+        batch_chunks = chunks[start: end]
+        batch_metadata = metadata[start:end]
+
+        response = client.embeddings.create(
+            model="text-embedding-3-small",
+            input=batch_chunks
+        )
+        batch_embeddings = [item.embedding for item in response.data]
+
+        for i, (embedding, metadata) in enumerate(zip(batch_embeddings, batch_metadata)):
+            vector_id = f"{metadata['pdf_id']}_chunk_{metadata['chunk_index']}"
+
+            vectors.append({"id": vector_id, "values": embedding, "metadata": metadata})
+
+    return vectors
+
+
